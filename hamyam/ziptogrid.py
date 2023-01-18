61c05363fd77b7d650eb346ef7cba26815594d79
Red Ziogas <ajz1369@rit.edu>
2023-01-18 01:33:12 -0500

    Document ziptogrid with comments

--- hamyam/ziptogrid.py
+++ hamyam/ziptogrid.py
@@ -3,29 +3,35 @@ import zipcodes
 import maidenhead as mh
 
 def ziptogrid(zipcode, config):
-	# Quick validity check
+	# Convert a United States Zicode to a Maidenhead Gridsquare location
+	# Takes: A US Zipcode
+	# Returns: interactions Embed object
+
 	message = interactions.Embed(
 		title="**__Zipcode to Gridsquare__**",
 		description=zipcode,
 		color=7368816
 	)
-	try:
-		int(zipcode)
 
+	# Quick validity checks
+	try:
+		int(zipcode) # Make sure the entered Zipcode is only digits
 	except ValueError:
 		message.add_field("Error", "The entered Zipcode does not appear to be a valid United States Zipcode.")
 		return message
-
 	if len(zipcode) != 5:
+		# Length check, all US Zipcodes are 5 digits
 		message.add_field("Error", "The entered Zipcode does not appear to be a valid United States Zipcode.")
 		return message
 
 	try:
+		# Search for zipcode
 		zipParsed = zipcodes.matching(zipcode)
 		if len(zipParsed) == 0:
+			# If no zipcode is found, it's probably invalid.
 			message.add_field("Error", "The entered Zipcode was unable to be located. Is it a valid United States Zipcode?")
 			return message
-
+	# Another validity check incase all others failed
 	except ValueError:
 		message.add_field("Error", "The entered Zipcode does not appear to be a valid United States Zipcode.")
 		return message
@@ -34,9 +40,9 @@ def ziptogrid(zipcode, config):
 	lon = float(zipParsed[0]['long'])
 	state = zipParsed[0]['state']
 	city = zipParsed[0]['city']
-
+	# Convert lat/long to gridsquare
 	gridsquare = mh.to_maiden(lat, lon)
-
+	# Create properly formatted interactions embed
 	message.add_field("Location", "{0}, {1} {2}".format(city, state, zipcode))
 	message.add_field("Gridsquare", "[{1}]({0}?grid={1})".format(config.config["GRIDSQUARE_URL"], gridsquare))
 	message.add_field("Coordinates", "[{0}, {1}]({2}?mlat={0}&mlon={1}&zoom=12)".format(lat, lon, config.config["OSM_URL"]))
