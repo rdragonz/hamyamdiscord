8c4be86ee7a01fa810bbdb1cba0f56f3867adb10
Red Ziogas <ajz1369@rit.edu>
2024-07-07 05:23:14 -0400

    Revert

--- hamyambot.py
+++ hamyambot.py
@@ -98,7 +98,7 @@ async def _conditions(ctx: interactions.CommandContext):
 @client.command(name="muf", description=config_data.lang["DESCRIPTION_MUF"], scope=config_data.config["GUILD_IDS"])
 async def _muf(ctx: interactions.CommandContext):
 	embed, image = hamyam.muf.muf(config_data)
-	await ctx.send(embeds=embed, files=image)
+	await ctx.send(embeds=embed, file=image)
 
 # /bands
 @client.command(name="bands", description=config_data.lang["DESCRIPTION_BANDS"], scope=config_data.config["GUILD_IDS"])
